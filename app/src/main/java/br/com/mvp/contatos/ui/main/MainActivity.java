9e776e9d26aa0a007e8fed31615b1a1503da3608
Wagner M A Santos <wagnercct2@gmail.com>
2019-05-01 17:14:00 -0300

    Inicio da aula 7

--- app/src/main/java/br/com/mvp/contatos/ui/main/MainActivity.java
+++ app/src/main/java/br/com/mvp/contatos/ui/main/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import br.com.mvp.contatos.R;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
